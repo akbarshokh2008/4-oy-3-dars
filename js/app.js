5f33fd2899b69d1b74777de5fa43af2bc305def5
akbarshokh2008 <mosinovakbarjon9@gmail.com>
2024-03-02 20:00:56 +0500

    ..

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// let operator, raqam1, raqam2;
+
+// operator = prompt("operator kiriting - masalan ( +  -  *  /  % )");
+// raqam1 = prompt("birinchi raqam kiriting");
+// raqam2 = prompt("ikkinchi raqam kiriting");
+
+// raqam1 = parseFloat(raqam1);
+// raqam2 = parseFloat(raqam2);
+
+// switch (operator) {
+//   case "+":
+//     console.log(`${raqam1} + ${raqam2} = ${raqam1 + raqam2}`);
+//     break;
+//   case "-":
+//     console.log(`${raqam1} - ${raqam2} = ${raqam1 - raqam2}`);
+//     break;
+//   case "*":
+//     console.log(`${raqam1} * ${raqam2} = ${raqam1 * raqam2}`);
+//     break;
+//   case "/":
+//     console.log(`${raqam1} / ${raqam2} = ${raqam1 / raqam2}`);
+//     break;
+
+//   case "%":
+//     console.log(`${raqam1} % ${raqam2} = ${raqam1 % raqam2}`);
+//     break;
+// }
