d14143d359e11f56c36cc73b49c80fa1185445a8
akbarshokh2008 <mosinovakbarjon9@gmail.com>
2024-03-05 21:53:47 +0500

    ss

--- js/app.js
+++ js/app.js
@@ -1,27 +1,78 @@
-// let operator, raqam1, raqam2;
+// 10- MASALA
+// let tomon1 = +prompt("uchburchakning birinchi tomonini kiriting");
+// let tomon2 = +prompt("uchburchakning ikkinchi tomonini kiriting");
+// let tomon3 = +prompt("uchburchakning uchunchi tomonini kiriting");
 
-// operator = prompt("operator kiriting - masalan ( +  -  *  /  % )");
-// raqam1 = prompt("birinchi raqam kiriting");
-// raqam2 = prompt("ikkinchi raqam kiriting");
+// function formator(tom1, tom2) {
+//   let result = `${tom1} + ${tom2} - 1 = ${tom1 + tom2 - 1}`;
 
-// raqam1 = parseFloat(raqam1);
-// raqam2 = parseFloat(raqam2);
-
-// switch (operator) {
-//   case "+":
-//     console.log(`${raqam1} + ${raqam2} = ${raqam1 + raqam2}`);
-//     break;
-//   case "-":
-//     console.log(`${raqam1} - ${raqam2} = ${raqam1 - raqam2}`);
-//     break;
-//   case "*":
-//     console.log(`${raqam1} * ${raqam2} = ${raqam1 * raqam2}`);
-//     break;
-//   case "/":
-//     console.log(`${raqam1} / ${raqam2} = ${raqam1 / raqam2}`);
-//     break;
-
-//   case "%":
-//     console.log(`${raqam1} % ${raqam2} = ${raqam1 % raqam2}`);
-//     break;
+//   return result;
 // }
+// console.log(formator(tomon1, tomon2, tomon3));
+
+// 11-MASALA
+// let son1 = +prompt("birinchi sonni kiriting");
+// let son2 = +prompt("ikkinchi sonni kiriting");
+
+// function formator(son1, son2) {
+//   let result = `${son1} % ${son2} = ${son1 % son2}`;
+
+//   return result;
+// }
+// console.log(formator(son1, son2));
+
+// 12- MASALA
+// let boyi = +prompt("boyini kiriting");
+// let eni = +prompt("enini kiriting");
+
+// function formator(x, y) {
+//   let result = `${x} * ${y} = ${x * y}`;
+
+//   return result;
+// }
+// console.log(formator(boyi, eni));
+
+// 13- MASALA
+// let matn = prompt("matn kiriting");
+
+// function stringQoshish(a) {
+//   let result = "Something" + " " + matn;
+
+//   return result;
+// }
+// console.log(stringQoshish(matn));
+
+// 14- MASALA
+// let raqam = prompt("raqam kiriting");
+
+// function formator(son) {
+//   let result = `${son} * ${son}  = ${son * son}`;
+
+//   return result;
+// }
+// console.log(formator(raqam));
+
+// 15 -MASALA
+// let son = +prompt("son kiriting");
+
+// function formator(son) {
+//   if (son >= 0) {
+//     console.log("true");
+//   } else {
+//     console.log("false");
+//   }
+
+//   return result;
+// }
+// console.log(formator(son));
+
+// 16 -MASALA
+
+// let son = +prompt("son kiriting");
+
+// function formator(n) {
+//   let result = `(${n} - 2) * 180  = ${(n - 2) * 180}`;
+
+//   return result;
+// }
+// console.log(formator(son));
